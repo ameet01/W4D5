0acd517a2581394bea33e7312c3128067ac50a5f
Ameet Vadhia <ameet01@gmail.com>
2017-10-27 10:32:44 -0700

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+  after_initialization :ensure_session_token
+
+  validates :session_token, :email, presence: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(email, password)
+    @user = User.find_by(email: email)
+    return nil if @user.nil?
+    if @user.valid_password?(password)
+      return @user
+    else
+      return nil
+    end
+  end
+end
